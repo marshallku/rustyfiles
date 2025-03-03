75fd2167cf1fbcdeb6dd2aef1897fbcc5030adf6
Marshall Ku <marshall@kakao.com>
2025-03-04 08:04:48 +0900

    Remove deprecated message

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,6 @@ async fn main() {
     let listener = TcpListener::bind(addr.as_str()).await.unwrap();
 
     info!("Server running at http://{}", addr);
-    error!("This package is deprecated, please use `ghcr.io/marshallku/rustyfiles:latest`: https://github.com/marshallku/rustyfiles");
 
     axum::serve(listener, app.into_make_service())
         .await
