486e1b48635f453d2a3d4394a96a92d30d5bed8f
Marshall Ku <marshall@kakao.com>
2025-08-08 13:07:06 +0900

    Add logics for serving images with hosts

--- src/controllers/images.rs
+++ src/controllers/images.rs
@@ -3,17 +3,15 @@ use axum::{
     response::IntoResponse,
 };
 
-use std::path::PathBuf;
-
 use crate::{
-    constants::CDN_ROOT, env::state::AppState, services::image::process_image_request,
-    utils::http::response_error,
+    env::state::AppState,
+    services::image::process_image_request,
+    utils::{http::response_error, path::parse_path},
 };
 
 pub async fn get(State(state): State<AppState>, Path(path): Path<String>) -> impl IntoResponse {
-    let file_path = PathBuf::from(format!("{}/images/{}", CDN_ROOT, path));
-
-    match process_image_request(&state, &path, &file_path).await {
+    let (host, path) = parse_path(&path);
+    match process_image_request(&state, host, &path).await {
         Ok(response) => response,
         Err(status) => response_error(status),
     }
--- src/services/image.rs
+++ src/services/image.rs
@@ -12,26 +12,34 @@ use crate::{
         http::response_file,
         img::{save_image_to_webp, save_resized_image},
         path::{get_original_path, get_resize_width_from_path},
+        url::get_host_from_url,
     },
 };
 
 pub async fn process_image_request(
     state: &AppState,
+    host: Option<String>,
     path: &str,
-    file_path: &PathBuf,
 ) -> Result<Response, StatusCode> {
+    let target_host = host.unwrap_or(state.host.clone());
+    let pure_host = get_host_from_url(&target_host);
+    let file_path = PathBuf::from(format!("{}/images/{}/{}", CDN_ROOT, pure_host, path));
+
     if file_path.exists() {
         error!("File exists but respond with Rust: {:?}", file_path);
-        return Ok(response_file(file_path).await);
+        return Ok(response_file(&file_path).await);
     }
 
     let resize_width = get_resize_width_from_path(path);
     let convert_to_webp = path.ends_with(".webp");
     let original_path = get_original_path(path, resize_width.is_some());
-    let original_file_path = PathBuf::from(format!("{}/images/{}", CDN_ROOT, original_path));
+    let original_file_path = PathBuf::from(format!(
+        "{}/images/{}/{}",
+        CDN_ROOT, pure_host, original_path
+    ));
 
     if !original_file_path.exists()
-        && fetch_and_cache(state.host.clone(), &original_file_path, &original_path)
+        && fetch_and_cache(target_host, &original_file_path, &original_path)
             .await
             .is_err()
     {
@@ -39,24 +47,27 @@ pub async fn process_image_request(
     }
 
     if resize_width.is_none() && !convert_to_webp {
-        return Ok(response_file(file_path).await);
+        return Ok(response_file(&file_path).await);
     }
 
     let image = open_image(&original_file_path)?;
 
     if !convert_to_webp {
-        return Ok(save_resized_image(image, resize_width, &original_file_path, file_path).await);
+        return Ok(save_resized_image(image, resize_width, &original_file_path, &file_path).await);
     }
 
     let path_with_webp = format!("{}.webp", original_path);
-    let file_path_with_webp = PathBuf::from(format!("{}/images/{}", CDN_ROOT, path_with_webp));
+    let file_path_with_webp = PathBuf::from(format!(
+        "{}/images/{}/{}",
+        CDN_ROOT, pure_host, path_with_webp
+    ));
 
     save_image_to_webp(&image, &file_path_with_webp)
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     let image_webp = open_image(&file_path_with_webp)?;
 
-    Ok(save_resized_image(image_webp, resize_width, &file_path_with_webp, file_path).await)
+    Ok(save_resized_image(image_webp, resize_width, &file_path_with_webp, &file_path).await)
 }
 
 fn open_image(file_path: &PathBuf) -> Result<DynamicImage, StatusCode> {
